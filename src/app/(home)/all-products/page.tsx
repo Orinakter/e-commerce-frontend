6514414cb274d10275de6f35c3fa491e298a2421
MeghlaB <meghlab964@gmail.com>
2025-04-07 23:53:32 +0600

    dashboard topbar added

---
+++ src/app/(home)/all-products/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+      all products
+    </div>
+  )
+}
+
+export default page
---
+++ src/app/(home)/wish/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+      wish
+    </div>
+  )
+}
+
+export default page
